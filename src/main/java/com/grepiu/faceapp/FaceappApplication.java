fa841a2ef02773b7861be29320f998946a573e81
iukim21c <iukim21c@smartlo.net>
2020-08-18 12:22:36 +0900

    1. 프레임웍 set

---
+++ src/main/java/com/grepiu/faceapp/FaceappApplication.java
@@ -0,0 +1,13 @@
+package com.grepiu.faceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grepiu/faceapp/FaceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grepiu.faceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
